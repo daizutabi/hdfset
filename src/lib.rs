cf273b8a00849d2997ab90e5f8176f2a09f274e6
Daizu <daizutabi@gmail.com>
2024-12-21 02:33:42 +0000

    Delete so

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-pub fn add(left: u64, right: u64) -> u64 {
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn add(left: u64, right: u64) -> u64 {
     left + right
+}
+
+#[pymodule]
+fn hdfset(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(add, m)?)?;
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -7,8 +17,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_add() {
+        assert_eq!(add(2, 2), 4);
     }
 }
